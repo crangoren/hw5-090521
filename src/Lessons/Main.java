c558b7e5f01a02b430c9eabf6160b2f9a7f5e85c
crangoren <crangoren@gmail.com>
2021-05-10 17:00:13 +0300

    initial commit

--- src/Lessons/Main.java
+++ src/Lessons/Main.java
@@ -3,10 +3,70 @@ package Lessons;
 import java.util.Arrays;
 
 public class Main {
+    static final int size = 10000000;
+    static final int h = size / 2;
 
 
+    public static void main(String[] args) throws InterruptedException {
 
-    public static void main(String[] args) {
+        calculate1();
+        calculate2();
+
+    }
+
+    public static void calculate1() {
+        float[] arr1 = new float[size];
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = 1.0f;
+        }
+
+        long a = System.currentTimeMillis();
+        for (int j = 0; j < arr1.length; j++) {
+            arr1[j] = (float)(arr1[j] * Math.sin(0.2f + j / 5) * Math.cos(0.2f + j / 5) * Math.cos(0.4f + j / 2));
+        }
+
+        System.currentTimeMillis();
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+    public static void calculate2() throws InterruptedException {
+        float[] arr1 = new float[size];
+        float[] a1 = new float[size/2];
+        float[] a2 = new float[size/2];
+
+        for (int k = 0; k < arr1.length; k++) {
+            arr1[k] = 1.0f;
+        }
+        System.currentTimeMillis();
+        long b = System.currentTimeMillis();
+
+        System.arraycopy(arr1, 0, a1, 0, h);
+        System.arraycopy(arr1, h, a2, 0, h);
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < a1.length; i++) {
+                a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int j = 0; j < a2.length; j++) {
+                a2[j] = (float)(a2[j] * Math.sin(0.2f + j / 5) * Math.cos(0.2f + j / 5) * Math.cos(0.4f + j / 2));
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+
+        System.arraycopy(a1, 0, arr1, 0, h);
+        System.arraycopy(a2, 0, arr1, h, h);
+
+        thread1.join();
+        thread2.join();
+
+        System.currentTimeMillis();
+        System.out.println(System.currentTimeMillis() - b);
 
     }
 
